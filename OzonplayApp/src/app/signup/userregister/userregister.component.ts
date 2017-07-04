0c1be1277e7ab57d7d35fdf7e5e0e7a92fcf611b
Ajit Rana <ajit.thakurs210@gmail.com>
2017-07-05 00:29:46 +0530

    final all things are done

--- OzonplayApp/src/app/signup/userregister/userregister.component.ts
+++ OzonplayApp/src/app/signup/userregister/userregister.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+
+declare let $:any;
 
 @Component({
   selector: 'app-userregister',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./userregister.component.css']
 })
 export class UserregisterComponent implements OnInit {
+  public pkg:any = {
+    "free":"0$ Free",
+    "lite":"5$ Lite 1 Month",
+    "sliver":"15$ Sliver 3 Months",
+    "gold":"25$ Gold 12 Months"
+  };
 
-  constructor() { }
+  public type:string = "loading....";
+
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit() {
+    $("html, body").animate({
+      scrollTop: 0
+    },200);
+    let val = this.route.snapshot.params["type"];
+    this.type = this.pkg[val];
   }
 
 }
